bb2dd244d51610ff889db573c667a00f500bb6d3
Art <moo@Mook2.local>
2018-11-27 22:47:46 -0500

    ls alias. install script

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -x
+cd "$(dirname "${BASH_SOURCE[0]}")";
+
+git pull origin master;
+
+function deployDotfiles() {
+	rsync --exclude=".git/" \
+    --exclude="README.md" \
+		--exclude="install.sh" \
+		-acvzh --no-perms . ~;
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	deployDotfiles;
+else
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	echo "";
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		deployDotfiles;
+	fi;
+fi;
+source ~/.bash_profile;
+unset deployDotfiles;
